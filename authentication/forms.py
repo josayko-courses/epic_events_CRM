8b14fe5d125cdc7c1f540d2af4bac1b09cc9dae0
Jonny Saykosy <josayko@pm.me>
2022-05-01 14:35:27 +0200

    Test a simple login form

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 from authentication.models import User
@@ -13,3 +14,8 @@ class MyUserChangeForm(UserChangeForm):
     class Meta:
         model = User
         fields = ("email", "first_name", "last_name")
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(max_length=256, widget=forms.EmailInput)
+    password = forms.CharField(max_length=64, widget=forms.PasswordInput)
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from authentication.forms import LoginForm
+from django.contrib.auth import login, authenticate
+
+
+def login_page(request):
+    form = LoginForm()
+    message = ""
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                email=form.cleaned_data["email"],
+                password=form.cleaned_data["password"],
+            )
+            if user is not None:
+                login(request, user)
+                message = f"Hello {user.email}! You have been logged in"
+            else:
+                message = "Login failed!"
+    context = {"form": form, "message": message}
+    return render(request, "login.html", context)
