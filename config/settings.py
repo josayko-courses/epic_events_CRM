49b35708ca031ec3717c1c2bc20160c37c4718b7
Jonny Saykosy <josayko@pm.me>
2022-05-15 10:50:58 +0200

    Add sentry to project

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,21 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://0c40317087664aa596bc1ab4783e0e37@o1247289.ingest.sentry.io/6407259",
+    integrations=[DjangoIntegration()],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from authentication import views
 
+
+def test(request):
+    division = 1 / 0
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("", views.login_page, name="login"),
     path("crm/", include("crm.urls")),
+    path("test/", test),
 ]
